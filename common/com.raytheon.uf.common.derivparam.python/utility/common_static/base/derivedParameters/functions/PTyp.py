33944143b8e54326b5f86ddca77165f2f61e5351
Matthew Foster <Matthew.Foster@noaa.gov>
2015-03-16 12:47:53 -0500

    VLab Issue #6909 - Add 3 Derived Parameter Functions; fixes #6909
    Change-Id: Ibc4fa162b1c4bbd7a9734a4454567e6de4bab0d5

--- common/com.raytheon.uf.common.derivparam.python/utility/common_static/base/derivedParameters/functions/PTyp.py
+++ common/com.raytheon.uf.common.derivparam.python/utility/common_static/base/derivedParameters/functions/PTyp.py
@@ -33,16 +33,22 @@ def execute2(CRAIN, CFRZR, CICEP, CSNOW):
     tmp = 1.0*CRAIN+2.0*CFRZR+4.0*CICEP+8.0*CSNOW
     # convert numeric value into symbol
     tmp[tmp <= 0] = 0
-    tmp[tmp > 9] = 0
+    tmp[tmp > 15] = 0
     tmp[tmp == 1] = 79
-    tmp[tmp == 2] = 89
+    tmp[tmp == 2] = 71
     tmp[tmp == 3] = 71
-    tmp[tmp == 4] = 77
-    tmp[tmp == 5] = 80
-    tmp[tmp == 6] = 67
-    tmp[tmp == 7] = 69
-    tmp[tmp == 8] = 47
-    tmp[tmp == 9] = 183
+    tmp[tmp == 4] = 80
+    tmp[tmp == 5] = 183
+    tmp[tmp == 6] = 185
+    tmp[tmp == 7] = 185
+    tmp[tmp == 8] = 89
+    tmp[tmp == 9] = 47
+    tmp[tmp == 10] = 186
+    tmp[tmp == 11] = 186
+    tmp[tmp == 12] = 184
+    tmp[tmp == 13] = 188
+    tmp[tmp == 14] = 187
+    tmp[tmp == 15] = 187
     return tmp
 
 def execute3(CRAIN6hr, CFRZR6hr, CICEP6hr, CSNOW6hr):
