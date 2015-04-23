f2ae59c04d008622999ee6120ad53b728e8499eb
Ron Anderson <ron.anderson@raytheon.com>
2015-04-23 13:05:08 -0500

    Omaha #4383 Fix RollBackImporter to roll back everything
    Change-Id: I82b2ab6687ec0d6e8d28a356d7cb4cb6146fc345

--- common/com.raytheon.uf.common.python/utility/common_static/base/python/RollBackImporter.py
+++ common/com.raytheon.uf.common.python/utility/common_static/base/python/RollBackImporter.py
@@ -1,40 +1,60 @@
-import sys, __builtin__, LogStream, re
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+# 
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+# 
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+# 
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+#    
+#     SOFTWARE HISTORY
+#    
+#    Date            Ticket#       Engineer       Description
+#    ------------    ----------    -----------    --------------------------
+#    04/23/2015      4383          randerso       Changed to roll back everything, 
+#                                                 not just non-base files
+#
+##
+
+import sys, __builtin__, LogStream
 
 class RollBackImporter:
     def __init__(self):
         "Creates an instance and installs as the global importer"
-        self.previousModules = sys.modules.copy()
+        self.previousModules = set(sys.modules.keys())
         self.realImport = __builtin__.__import__
         __builtin__.__import__ = self._import
-        self.newModules = {}
-        self.pattern = re.compile("/.*?/edex/data/utility/.*?/(.*?)/")
+        self.newModules = set()
 
     def _import(self, name, globals=None, locals=None, fromlist=[], level=-1):
         result = self.realImport(name, globals, locals, fromlist, level)
         
-        if hasattr(result, '__file__'):
-            match = re.match(self.pattern, result.__file__)
-            if match is not None:
-                level = match.group(1)
-                if level != 'base':
-#                    LogStream.logDebug("IMPORTING:", name, result)
-                    self.newModules[result.__name__] = 1
-#                else:
-#                    LogStream.logDebug("IGNORING BASE:", name, result)
-#            else:
-#                LogStream.logDebug("IGNORING NON-LOCALIZED:", name, result)
-#        else:
-#            LogStream.logDebug("IGNORING BUILTIN:", name, result)
+        # add to list of modules to be rolled back
+        self.newModules.add(result.__name__)
+
         return result
 
     def rollback(self):
-        for modname in self.newModules.keys():
-            if not self.previousModules.has_key(modname):
+        for modname in self.newModules:
+            if modname not in self.previousModules:
                 # Force reload when modname next imported
-#                LogStream.logDebug("UNLOADING:", modname, sys.modules[modname])
-                del(sys.modules[modname])
-#            else:
-#                LogStream.logDebug("SKIPPING PRELOADED:", modname)
+                if sys.modules.has_key(modname):
+#                     LogStream.logDebug("UNLOADING:", modname)
+                    del(sys.modules[modname])
+#                 else:
+#                     LogStream.logDebug("MODULE NOT FOUND:", modname)
+#             else:
+#                 LogStream.logDebug("SKIPPING PRELOADED:", modname)
 
-        self.newModules = {}
-                
+        self.newModules.clear()
