0b2bc9894758aa7330c1688762905c256ef02376
Richard Peter <richard.peter@raytheon.com>
2015-07-13 14:46:24 -0500

    Omaha #4500 - Remove use of executeNativeSql
    Change-Id: Idaf0e47f45afaadcccdf18b24bfd1d1b8eaeb787

--- common/com.raytheon.uf.common.dataquery/src/com/raytheon/uf/common/dataquery/requests/QlServerRequest.java
+++ common/com.raytheon.uf.common.dataquery/src/com/raytheon/uf/common/dataquery/requests/QlServerRequest.java
@@ -26,7 +26,7 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 import com.raytheon.uf.common.serialization.comm.IServerRequest;
 
 /**
- * IServerRequest object for making a sql or hql request
+ * IServerRequest object for making a sql or hql request.
  * 
  * <pre>
  * 
@@ -35,7 +35,7 @@ import com.raytheon.uf.common.serialization.comm.IServerRequest;
  * ------------- -------- ----------- --------------------------
  * Feb 16, 2011  8070     ekladstrup  Initial creation
  * Dec 18, 2013  2579     bsteffen    Remove ISerializableObject
- * 
+ * Jul 13, 2015  4500     rjpeter     Added paramMap, demystified interface.
  * </pre>
  * 
  * @author ekladstrup
@@ -43,29 +43,137 @@ import com.raytheon.uf.common.serialization.comm.IServerRequest;
  */
 @DynamicSerialize
 public class QlServerRequest implements IServerRequest {
+    /** The language of the query */
+    public static enum QueryLanguage {
+        SQL, HQL
+    };
+
+    /** The language of the query */
+    public static enum QueryType {
+        QUERY, STATEMENT
+    };
+
+    /**
+     * The language to use. If not specified will default to sql.
+     */
+    @DynamicSerializeElement
+    private QueryLanguage lang;
+
+    /**
+     * The type the of sql/hql. If not specified will default to query.
+     */
+    @DynamicSerializeElement
+    private QueryType type;
+
+    /**
+     * The database to run the query against. If not specified will default to
+     * metadata.
+     */
+    @DynamicSerializeElement
+    private String database;
 
     @DynamicSerializeElement
-    private Map<String, RequestConstraint> rcMap;
+    private String query;
+
+    @DynamicSerializeElement
+    private Map<String, Object> paramMap;
 
     public QlServerRequest() {
-
     }
 
-    public QlServerRequest(Map<String, RequestConstraint> rcMap) {
-        this.rcMap = rcMap;
+    public QlServerRequest(String query) {
+        this.query = query;
     }
 
-    public Map<String, RequestConstraint> getRcMap() {
-        return rcMap;
+    public QlServerRequest(String query, Map<String, Object> paramMap) {
+        this.query = query;
+        this.paramMap = paramMap;
     }
 
-    public void setRcMap(Map<String, RequestConstraint> map) {
-        rcMap = map;
+    /**
+     * @return the lang
+     */
+    public QueryLanguage getLang() {
+        return lang;
+    }
+
+    /**
+     * @param lang
+     *            the lang to set
+     */
+    public void setLang(QueryLanguage lang) {
+        this.lang = lang;
+    }
+
+    /**
+     * @return the type
+     */
+    public QueryType getType() {
+        return type;
+    }
+
+    /**
+     * @param type
+     *            the type to set
+     */
+    public void setType(QueryType type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the database
+     */
+    public String getDatabase() {
+        return database;
+    }
+
+    /**
+     * @param database
+     *            the database to set
+     */
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    /**
+     * @return the query
+     */
+    public String getQuery() {
+        return query;
+    }
+
+    /**
+     * @param query
+     *            the query to set
+     */
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    /**
+     * @return the paramMap
+     */
+    public Map<String, Object> getParamMap() {
+        return paramMap;
+    }
+
+    /**
+     * @param paramMap
+     *            the paramMap to set
+     */
+    public void setParamMap(Map<String, Object> paramMap) {
+        this.paramMap = paramMap;
     }
 
     @Override
     public String toString() {
-        return "QlServerRequest [rcMap=" + rcMap + "]";
+        StringBuilder msg = new StringBuilder(80);
+        msg.append("QlServerRequest [");
+        if (database != null) {
+            msg.append("database=").append(database).append(", ");
+        }
+        msg.append("query=").append(query).append(']');
+        return msg.toString();
     }
 
 }
