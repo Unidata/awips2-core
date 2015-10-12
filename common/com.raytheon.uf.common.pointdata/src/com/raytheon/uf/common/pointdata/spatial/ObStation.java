7cc3d49bd645a699e52f2534eb86096b341224c0
Richard Peter <richard.peter@raytheon.com>
2015-10-12 17:23:00 -0500

    Omaha #4911 - Remove unused columns from common_obs_spatial
    Change-Id: I73143fea55aed4e6ee6f51720c269abb783bb215

--- common/com.raytheon.uf.common.pointdata/src/com/raytheon/uf/common/pointdata/spatial/ObStation.java
+++ common/com.raytheon.uf.common.pointdata/src/com/raytheon/uf/common/pointdata/spatial/ObStation.java
@@ -53,8 +53,8 @@ import com.vividsolutions.jts.geom.Point;
  * Jul 24, 2007 353         bphillip    Initial Check in
  * Jul 16, 2013 2181        bsteffen    Convert geometry types to use hibernate-
  *                                      spatial
- * 10/16/2014   3454       bphillip    Upgrading to Hibernate 4                                  
- * 
+ * 10/16/2014   3454        bphillip    Upgrading to Hibernate 4
+ * Oct 12, 2015 4911        rjpeter     Removed unused columns.
  * </pre>
  * 
  * @author bphillip
@@ -64,386 +64,338 @@ import com.vividsolutions.jts.geom.Point;
 @Table(name = "common_obs_spatial")
 @XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
-public class ObStation extends PersistableDataObject implements ISpatialObject {
+public class ObStation extends PersistableDataObject<String> implements
+        ISpatialObject {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public static final Integer CAT_TYPE_ICAO = 1;
+    public static final Integer CAT_TYPE_ICAO = 1;
 
-	// Legacy SAO identifiers - Still some around
-	public static final Integer CAT_TYPE_SAO = 2;
+    // Legacy SAO identifiers - Still some around
+    public static final Integer CAT_TYPE_SAO = 2;
 
-	// National Weather Service Weather Forecast Office (WFO) locations.
-	public static final Integer CAT_TYPE_WFOID = 10;
+    // National Weather Service Weather Forecast Office (WFO) locations.
+    public static final Integer CAT_TYPE_WFOID = 10;
 
-	// National Weather Service NEXRAD radar sites.
-	public static final Integer CAT_TYPE_NEXRAD = 11;
+    // National Weather Service NEXRAD radar sites.
+    public static final Integer CAT_TYPE_NEXRAD = 11;
 
-	// Profiler site locations
-	public static final Integer CAT_TYPE_PROF = 12;
+    // Profiler site locations
+    public static final Integer CAT_TYPE_PROF = 12;
 
-	// WMO fixed surface synoptic locations
-	public static final Integer CAT_TYPE_SFC_FXD = 20;
+    // WMO fixed surface synoptic locations
+    public static final Integer CAT_TYPE_SFC_FXD = 20;
 
-	// WMO mobil surface synoptic locations
-	public static final Integer CAT_TYPE_SFC_MOB = 21;
+    // WMO mobil surface synoptic locations
+    public static final Integer CAT_TYPE_SFC_MOB = 21;
 
-	// WMO fixed upperair locations
-	public static final Integer CAT_TYPE_SFC_RAOB = 22;
+    // WMO fixed upperair locations
+    public static final Integer CAT_TYPE_SFC_RAOB = 22;
 
-	// Known ship identifications - Mobile no lat/lon
-	public static final Integer CAT_TYPE_SHIP_MOB = 30;
+    // Known ship identifications - Mobile no lat/lon
+    public static final Integer CAT_TYPE_SHIP_MOB = 30;
 
-	// Drifting buoy locations
-	public static final Integer CAT_TYPE_BUOY_MOB = 31;
+    // Drifting buoy locations
+    public static final Integer CAT_TYPE_BUOY_MOB = 31;
 
-	// Moored (Fixed) buoy locations
-	public static final Integer CAT_TYPE_BUOY_FXD = 32;
+    // Moored (Fixed) buoy locations
+    public static final Integer CAT_TYPE_BUOY_FXD = 32;
 
-	// Coastal Marine (CMAN) locations.
-	public static final Integer CAT_TYPE_CMAN = 33;
+    // Coastal Marine (CMAN) locations.
+    public static final Integer CAT_TYPE_CMAN = 33;
 
-	// Aircraft waypoint locations
-	public static final Integer CAT_TYPE_ACFT_WAYPT = 100;
+    // Aircraft waypoint locations
+    public static final Integer CAT_TYPE_ACFT_WAYPT = 100;
 
-	// Aircraft pirep locations
-	public static final Integer CAT_TYPE_ACFT_PIREP = 101;
+    // Aircraft pirep locations
+    public static final Integer CAT_TYPE_ACFT_PIREP = 101;
 
-	// Base catagory type for US mesonet stations.
-	// Subnets may be defined in the range 1000-1999 by ading this value
-	// to the Mesonet id;
-	// Example Iowa DOT, IADOT = 91 so the subnet id = 1091
-	//
-	public static final Integer CAT_TYPE_MESONET = 1000;
+    // Base catagory type for US mesonet stations.
+    // Subnets may be defined in the range 1000-1999 by ading this value
+    // to the Mesonet id;
+    // Example Iowa DOT, IADOT = 91 so the subnet id = 1091
+    //
+    public static final Integer CAT_TYPE_MESONET = 1000;
 
-	public static final Integer MESONET_NWSFAA = 1001;
+    public static final Integer MESONET_NWSFAA = 1001;
 
-	@Id
-	@Column(length = 32)
-	@Index(name = "gidIndex")
-	@DynamicSerializeElement
-	private String gid;
+    @Id
+    @Column(length = 32)
+    @Index(name = "gidIndex")
+    @DynamicSerializeElement
+    private String gid;
 
-	/** The icao */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 16)
-	@Index(name = "icaoIndex")
-	private String icao;
+    /** The icao */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column(length = 16)
+    @Index(name = "icaoIndex")
+    private String icao;
 
-	/** The WMO index */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column
-	private Integer wmoIndex;
+    /** The WMO index */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column
+    private Integer wmoIndex;
 
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 16, nullable = false)
-	private String stationId;
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column(length = 16, nullable = false)
+    private String stationId;
 
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(nullable = false)
-	private Integer catalogType;
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column(nullable = false)
+    private Integer catalogType;
 
-	/** Descriptive name of the station */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column
-	private String name;
+    /** Descriptive name of the station */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column
+    private String name;
 
-	/** Country location */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 32)
-	private String country;
+    /** Country location */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column(length = 32)
+    private String country;
 
-	/** State location (if applicable) */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 32)
-	private String state;
+    /** State location (if applicable) */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column(length = 32)
+    private String state;
 
-	/** The WMO region */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column
-	private Integer wmoRegion;
+    /** The WMO region */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column
+    private Integer wmoRegion;
 
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 16)
-	private String pressureLevel;
+    /** The elevation of the station */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column
+    private Integer elevation;
 
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 1)
-	private String aerodromeFlag;
+    /** The upper air elevation */
+    @XmlAttribute
+    @DynamicSerializeElement
+    @Column
+    private Integer upperAirElevation;
 
-	/** The elevation of the station */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column
-	private Integer elevation;
-
-	/** The upper air elevation */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column
-	private Integer upperAirElevation;
-
-	/** The RBSN Indicator */
-	@XmlAttribute
-	@DynamicSerializeElement
-	@Column(length = 4)
-	private String rbsnIndicator;
-
-	/** The upper air geometry information */
+    /** The upper air geometry information */
     @Column(name = "upperairgeom")
     @Type(type = "org.hibernate.spatial.GeometryType")
-	@XmlJavaTypeAdapter(value = GeometryAdapter.class)
-	@DynamicSerializeElement
-	private Point upperAirGeometry;
+    @XmlJavaTypeAdapter(value = GeometryAdapter.class)
+    @DynamicSerializeElement
+    private Point upperAirGeometry;
 
-	/** The station location */
+    /** The station location */
     @Column(name = "the_geom")
     @Type(type = "org.hibernate.spatial.GeometryType")
-	@XmlJavaTypeAdapter(value = GeometryAdapter.class)
-	@DynamicSerializeElement
-	private Point location;
+    @XmlJavaTypeAdapter(value = GeometryAdapter.class)
+    @DynamicSerializeElement
+    private Point location;
 
-	/**
-	 * @return the catalogType
-	 */
-	public Integer getCatalogType() {
-		return catalogType;
-	}
+    /**
+     * @return the catalogType
+     */
+    public Integer getCatalogType() {
+        return catalogType;
+    }
 
-	/**
-	 * @param catalogType
-	 *            the catalogType to set
-	 */
-	public void setCatalogType(Integer catalogType) {
-		this.catalogType = catalogType;
-	}
+    /**
+     * @param catalogType
+     *            the catalogType to set
+     */
+    public void setCatalogType(Integer catalogType) {
+        this.catalogType = catalogType;
+    }
 
-	/**
-	 * @return the stationId
-	 */
-	public String getStationId() {
-		return stationId;
-	}
+    /**
+     * @return the stationId
+     */
+    public String getStationId() {
+        return stationId;
+    }
 
-	/**
-	 * @param stationId
-	 *            the stationId to set
-	 */
-	public void setStationId(String stationId) {
-		this.stationId = stationId;
-	}
+    /**
+     * @param stationId
+     *            the stationId to set
+     */
+    public void setStationId(String stationId) {
+        this.stationId = stationId;
+    }
 
-	@Override
-	public Point getGeometry() {
-		return location;
-	}
+    @Override
+    public Point getGeometry() {
+        return location;
+    }
 
-	@Override
-	public CoordinateReferenceSystem getCrs() {
-		return null;
-	}
+    @Override
+    public CoordinateReferenceSystem getCrs() {
+        return null;
+    }
 
-	public String getGid() {
-		return gid;
-	}
+    public String getGid() {
+        return gid;
+    }
 
-	public void setGid(String gid) {
-		this.gid = gid;
-	}
+    public void setGid(String gid) {
+        this.gid = gid;
+    }
 
-	public String getIcao() {
-		return icao;
-	}
+    public String getIcao() {
+        return icao;
+    }
 
-	public void setIcao(String icao) {
-		this.icao = icao;
-	}
+    public void setIcao(String icao) {
+        this.icao = icao;
+    }
 
-	public Integer getWmoIndex() {
-		return wmoIndex;
-	}
+    public Integer getWmoIndex() {
+        return wmoIndex;
+    }
 
-	public void setWmoIndex(Integer wmoIndex) {
-		this.wmoIndex = wmoIndex;
-	}
+    public void setWmoIndex(Integer wmoIndex) {
+        this.wmoIndex = wmoIndex;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getCountry() {
-		return country;
-	}
+    public String getCountry() {
+        return country;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-	public String getState() {
-		return state;
-	}
+    public String getState() {
+        return state;
+    }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	public Integer getWmoRegion() {
-		return wmoRegion;
-	}
+    public Integer getWmoRegion() {
+        return wmoRegion;
+    }
 
-	public void setWmoRegion(Integer wmoRegion) {
-		this.wmoRegion = wmoRegion;
-	}
+    public void setWmoRegion(Integer wmoRegion) {
+        this.wmoRegion = wmoRegion;
+    }
 
-	/**
-	 * @return the pressureLevel
-	 */
-	public String getPressureLevel() {
-		return pressureLevel;
-	}
+    public Integer getElevation() {
+        return elevation;
+    }
 
-	/**
-	 * @param pressureLevel
-	 *            the pressureLevel to set
-	 */
-	public void setPressureLevel(String pressureLevel) {
-		this.pressureLevel = pressureLevel;
-	}
+    public void setElevation(Integer elevation) {
+        this.elevation = elevation;
+    }
 
-	/**
-	 * @return the aerodromeFlag
-	 */
-	public String getAerodromeFlag() {
-		return aerodromeFlag;
-	}
+    public Integer getUpperAirElevation() {
+        return upperAirElevation;
+    }
 
-	/**
-	 * @param aerodromeFlag
-	 *            the aerodromeFlag to set
-	 */
-	public void setAerodromeFlag(String aerodromeFlag) {
-		this.aerodromeFlag = aerodromeFlag;
-	}
+    public void setUpperAirElevation(Integer upperAirElevation) {
+        this.upperAirElevation = upperAirElevation;
+    }
 
-	public Integer getElevation() {
-		return elevation;
-	}
+    public Point getUpperAirGeometry() {
+        return upperAirGeometry;
+    }
 
-	public void setElevation(Integer elevation) {
-		this.elevation = elevation;
-	}
+    public void setUpperAirGeometry(Point upperAirGeometry) {
+        this.upperAirGeometry = upperAirGeometry;
+    }
 
-	public Integer getUpperAirElevation() {
-		return upperAirElevation;
-	}
+    public Point getStationGeom() {
+        return location;
+    }
 
-	public void setUpperAirElevation(Integer upperAirElevation) {
-		this.upperAirElevation = upperAirElevation;
-	}
+    public void setStationGeom(Point stationGeom) {
+        this.location = stationGeom;
+    }
 
-	public String getRbsnIndicator() {
-		return rbsnIndicator;
-	}
+    @Override
+    public Integer getNx() {
+        return 0;
+    }
 
-	public void setRbsnIndicator(String rbsnIndicator) {
-		this.rbsnIndicator = rbsnIndicator;
-	}
+    @Override
+    public Integer getNy() {
+        return 0;
+    }
 
-	public Point getUpperAirGeometry() {
-		return upperAirGeometry;
-	}
+    /**
+     * @return the location
+     */
+    public Point getLocation() {
+        return location;
+    }
 
-	public void setUpperAirGeometry(Point upperAirGeometry) {
-		this.upperAirGeometry = upperAirGeometry;
-	}
+    /**
+     * @param location
+     *            the location to set
+     */
+    public void setLocation(Point location) {
+        this.location = location;
+    }
 
-	public Point getStationGeom() {
-		return location;
-	}
+    public static final String createGID(Integer catalogType, String stationId) {
+        String gid = null;
+        if ((catalogType != null) && (stationId != null)) {
+            gid = String.format("%04d-%s", catalogType, stationId);
+        }
+        return gid;
+    }
 
-	public void setStationGeom(Point stationGeom) {
-		this.location = stationGeom;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + ((gid == null) ? 0 : gid.hashCode());
+        return result;
+    }
 
-	@Override
-	public Integer getNx() {
-		return 0;
-	}
-
-	@Override
-	public Integer getNy() {
-		return 0;
-	}
-
-	/**
-	 * @return the location
-	 */
-	public Point getLocation() {
-		return location;
-	}
-
-	/**
-	 * @param location
-	 *            the location to set
-	 */
-	public void setLocation(Point location) {
-		this.location = location;
-	}
-
-	public static final String createGID(Integer catalogType, String stationId) {
-		String gid = null;
-		if ((catalogType != null) && (stationId != null)) {
-			gid = String.format("%04d-%s", catalogType, stationId);
-		}
-		return gid;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((gid == null) ? 0 : gid.hashCode());
-		return result;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ObStation other = (ObStation) obj;
-		if (gid == null) {
-			if (other.gid != null)
-				return false;
-		} else if (!gid.equals(other.gid))
-			return false;
-		return true;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ObStation other = (ObStation) obj;
+        if (gid == null) {
+            if (other.gid != null) {
+                return false;
+            }
+        } else if (!gid.equals(other.gid)) {
+            return false;
+        }
+        return true;
+    }
 }
