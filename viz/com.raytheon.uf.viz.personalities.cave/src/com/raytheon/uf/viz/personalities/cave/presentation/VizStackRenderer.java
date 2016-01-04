ef25359de6862dae9949117540383f7e21faed89
Ben Steffensmeier <Ben.Steffensmeier@raytheon.com>
2016-01-04 12:32:48 -0600

    Omaha #5188 Remove the '*' from renameable parts.

--- viz/com.raytheon.uf.viz.personalities.cave/src/com/raytheon/uf/viz/personalities/cave/presentation/VizStackRenderer.java
+++ viz/com.raytheon.uf.viz.personalities.cave/src/com/raytheon/uf/viz/personalities/cave/presentation/VizStackRenderer.java
@@ -19,17 +19,50 @@
  **/
 package com.raytheon.uf.viz.personalities.cave.presentation;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.e4.ui.model.application.ui.MElementContainer;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.renderers.swt.StackRenderer;
+import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.internal.e4.compatibility.CompatibilityEditor;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
 
 import com.raytheon.uf.viz.personalities.cave.menu.VizEditorSystemMenu;
+import com.raytheon.viz.ui.IRenameablePart;
 
 /**
  * Custom version of {@link StackRenderer} that allows the
  * {@link VizEditorSystemMenu} to add menu items whenever the right click menu
  * is opened for a tab with a {@link CompatibilityEditor}.
+ * 
+ * This also extends the tab naming convention so that instances of
+ * {@link IRenameablePart} do not prepend an '*' to the name. Determining if a
+ * part is renameable and dirty is complicated so there are 3 things needed to
+ * suppress the '*'
+ * <ol>
+ * <li>When a tab is created if it is dirty and renameable then the '*' is
+ * removed. In practice this doesn't seem to happen because a part is created
+ * 'clean' and only marked dirty during initialization, also the actual Object
+ * for the part is usually not set this early so it is impossible to tell if it
+ * is renameable.</li>
+ * <li>This class gets updates to the dirty flag of parts and if they are
+ * renameable it will ignore these updates so that no '*' is added. During
+ * initialization this doesn't work because the dirty flag can be set before the
+ * Object is set so we have no way of knowing if it is renameable.</li>
+ * <li>This class listens for when the object is set on a part and if the object
+ * is dirty and renameable then remove the '*', the object is usually set
+ * towards the end of initialization so this handles the flaw left in the
+ * previous methods.</li>
+ * </ol>
  * 
  * <pre>
  * SOFTWARE HISTORY
@@ -44,8 +77,39 @@ import com.raytheon.uf.viz.personalities.cave.menu.VizEditorSystemMenu;
 @SuppressWarnings("restriction")
 public class VizStackRenderer extends StackRenderer {
 
+    @Inject
+    private IEventBroker eventBroker;
+
+    /**
+     * Custom dirty rendering part 3: When the contribution item of a part is
+     * set remove the '*' if it is renameable.
+     */
+    private final EventHandler setContributionObjectHandler = new EventHandler() {
+
+        @Override
+        public void handleEvent(Event event) {
+            MUIElement element = (MUIElement) event
+                    .getProperty(UIEvents.EventTags.ELEMENT);
+            if (!(element instanceof MPart)) {
+                return;
+            }
+            Object attName = event.getProperty(UIEvents.EventTags.ATTNAME);
+            if (!UIEvents.Contribution.OBJECT.equals(attName)) {
+                return;
+            }
+            MPart part = (MPart) element;
+            if (isDirty(element) && isRenameable(part)) {
+                CTabItem tabItem = findItemForPart(part);
+                if (tabItem != null) {
+                    tabItem.setText(tabItem.getText().substring(1));
+                }
+            }
+        }
+    };
+
     private VizEditorSystemMenu editorMenu = null;
 
+    /** Override to add custom menu items */
     @Override
     protected void populateTabMenu(Menu menu, MPart part) {
         super.populateTabMenu(menu, part);
@@ -58,4 +122,62 @@ public class VizStackRenderer extends StackRenderer {
         }
     }
 
+    @PostConstruct
+    public void startSubscriptions() {
+        eventBroker.subscribe(UIEvents.Contribution.TOPIC_OBJECT,
+                setContributionObjectHandler);
+    }
+
+    @PreDestroy
+    public void stopSubscriptions() {
+        eventBroker.unsubscribe(setContributionObjectHandler);
+    }
+
+    /**
+     * Custom dirty rendering part 1: remove the '*' from dirty renameable parts
+     * when the widget is created.
+     */
+    @Override
+    protected void createTab(MElementContainer<MUIElement> stack,
+            MUIElement element) {
+        Object widget = element.getWidget();
+        super.createTab(stack, element);
+        if (widget == null && element instanceof MPart && isDirty(element)) {
+            MPart part = (MPart) element;
+            if (isRenameable(part)) {
+                CTabItem tabItem = findItemForPart(part);
+                if (tabItem != null) {
+                    tabItem.setText(tabItem.getText().substring(1));
+                }
+            }
+
+        }
+    }
+
+    /**
+     * Custom dirty rendering part 2: Ignore dirty events from renameable parts.
+     */
+    @Override
+    protected void updateTab(CTabItem cti, MPart part, String attName,
+            Object newValue) {
+        if (UIEvents.Dirtyable.DIRTY.equals(attName)) {
+            if (isRenameable(part)) {
+                return;
+            }
+        }
+        super.updateTab(cti, part, attName, newValue);
+    }
+
+    private static boolean isDirty(MUIElement element) {
+        return element instanceof MDirtyable
+                && ((MDirtyable) element).isDirty();
+    }
+
+    private static boolean isRenameable(MPart part) {
+        if (part.getObject() instanceof CompatibilityEditor) {
+            CompatibilityEditor editor = (CompatibilityEditor) part.getObject();
+            return (editor.getEditor() instanceof IRenameablePart);
+        }
+        return false;
+    }
 }
