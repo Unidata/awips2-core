ee6bbd6c8460b2d96378cb2227739b66be5584e8
Ron Anderson <ron.anderson@raytheon.com>
2016-10-26 15:25:33 -0500

    Omaha #5967 Fix map label font scaling issues
    Change-Id: I2419853045d7ab047ba26f12d3c9ad68a1c793cc

--- viz/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/internal/PreferenceBasedGLFont.java
+++ viz/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/internal/PreferenceBasedGLFont.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -30,18 +30,18 @@ import com.sun.opengl.util.j2d.TextRenderer;
 
 /**
  * GL font that is set by preferences page, listens for updates
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
+ *
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Oct 19, 2010            mschenke    Initial creation
  * Nov 04, 2015   5070     randerso    Added DPI font scaling
- * 
+ *
  * </pre>
- * 
+ *
  * @author mschenke
  * @version 1.0
  */
@@ -149,7 +149,7 @@ public class PreferenceBasedGLFont implements IGLFont, IPropertyChangeListener {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see
      * org.eclipse.jface.util.IPropertyChangeListener#propertyChange(org.eclipse
      * .jface.util.PropertyChangeEvent)
@@ -157,15 +157,21 @@ public class PreferenceBasedGLFont implements IGLFont, IPropertyChangeListener {
     @Override
     public void propertyChange(PropertyChangeEvent event) {
         if (propertyName.equals(event.getProperty())) {
+            boolean smoothing = preferenceFont.getSmoothing();
+            boolean scaleFont = preferenceFont.isScaleFont();
+            float magnification = preferenceFont.getMagnification();
             preferenceFont.disposeInternal();
             preferenceFont = FontFactory.getInstance().getFont(getDPI(),
                     propertyName);
+            preferenceFont.setSmoothing(smoothing);
+            preferenceFont.setScaleFont(scaleFont);
+            preferenceFont.setMagnification(magnification);
         }
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see com.raytheon.viz.core.gl.IGLFont#disposeInternal()
      */
     @Override
