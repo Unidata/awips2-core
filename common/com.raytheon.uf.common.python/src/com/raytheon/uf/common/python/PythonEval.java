d051c9a64541adbacb809fb065bdb761e799a2d3
Ron Anderson <ron.anderson@raytheon.com>
2017-01-24 11:13:07 -0600

    Omaha #6092 Add runScript method to PythonEval
    Change-Id: I836abee806ccae93797006eda88e653aa5253cb1

--- common/com.raytheon.uf.common.python/src/com/raytheon/uf/common/python/PythonEval.java
+++ common/com.raytheon.uf.common.python/src/com/raytheon/uf/common/python/PythonEval.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -26,21 +26,21 @@ import jep.JepException;
 
 /**
  * A PythonScript for evaluating python statements.
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Oct 05, 2011            njensen     Initial creation
- * Sep 19, 2012      #1091 randerso    Changed to extend PythonScript to 
- *                                     allow access to both eval and execute
- *                                     methods
- * Jan 05, 2017       5959 njensen     Use JepConfig in constructor
- * 
+ *
+ * Date          Ticket#  Engineer  Description
+ * ------------- -------- --------- --------------------------------------------
+ * Oct 05, 2011           njensen   Initial creation
+ * Sep 19, 2012  1091     randerso  Changed to extend PythonScript to allow
+ *                                  access to both eval and execute methods
+ * Jan 05, 2017  5959     njensen   Use JepConfig in constructor
+ * Jan 24, 2017  6092     randerso  Add runScript method
+ *
  * </pre>
- * 
+ *
  * @author njensen
  */
 
@@ -48,7 +48,7 @@ public class PythonEval extends PythonScript {
 
     /**
      * Constructor
-     * 
+     *
      * @param config
      *            the jep config to use with the interpreter
      * @throws JepException
@@ -59,7 +59,7 @@ public class PythonEval extends PythonScript {
 
     /**
      * Constructor
-     * 
+     *
      * @param config
      *            the jep config to use with the interpreter
      * @param preEvals
@@ -74,9 +74,9 @@ public class PythonEval extends PythonScript {
 
     /**
      * Constructor
-     * 
+     *
      * @deprecated use PythonEval(JepConfig) instead
-     * 
+     *
      * @param includePath
      * @param classLoader
      * @throws JepException
@@ -90,9 +90,9 @@ public class PythonEval extends PythonScript {
 
     /**
      * Constructor
-     * 
+     *
      * @deprecated Use PythonEval(JepConfig, List<String) instead
-     * 
+     *
      * @param includePath
      * @param classLoader
      * @param preEvals
@@ -111,6 +111,18 @@ public class PythonEval extends PythonScript {
 
     public Object getValue(String name) throws JepException {
         return this.jep.getValue(name);
+    }
+
+    /**
+     * Runs a Python script.
+     *
+     * @param script
+     *            a <code>String</code> absolute path to script file.
+     * @exception JepException
+     *                if an error occurs
+     */
+    public void runScript(String script) throws JepException {
+        jep.runScript(script);
     }
 
 }
