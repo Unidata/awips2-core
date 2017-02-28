e42daba9a97e3193655241b4a0eaec35ab7e08f9
Ron Anderson <ron.anderson@raytheon.com>
2017-02-28 09:43:14 -0600

    Omaha #6121 Change DualListConfig to specify list height in items and width in characters
    Change-Id: I7f85ef4acc6b15224a70884257725086a015a2a3

--- viz/com.raytheon.viz.ui/src/com/raytheon/viz/ui/widgets/duallist/DualListConfig.java
+++ viz/com.raytheon.viz.ui/src/com/raytheon/viz/ui/widgets/duallist/DualListConfig.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -22,16 +22,19 @@ package com.raytheon.viz.ui.widgets.duallist;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.eclipse.swt.SWT;
 
 /**
  * Config file for DualList class. Reused from Data Delivery.
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
+ *
  * Date          Ticket#  Engineer  Description
- * ------------- -------- --------- --------------------------------------
+ * ------------- -------- --------- --------------------------------------------
  * May 31, 2012           mpduff    Initial creation.
  * Aug 10, 2012  1002     mpduff    Added numeric flag for sorting.
  * Jan 07, 2013  1431     mpduff    Add case sensitive and exclude flags.
@@ -39,11 +42,12 @@ import java.util.List;
  * Sep 27, 2013  2419     lvenable  Updated include description.
  * May 04, 2015  4419     rferrel   Add {@link #sortList} flag.
  * May 05, 2016  5487     tjensen   Added option for reverse sorting.
- * 
+ * Feb 28, 2017  6121     randerso  Change DualListConfig to specify list height
+ *                                  in items and width in characters
+ *
  * </pre>
- * 
+ *
  * @author mpduff
- * @version 1.0
  */
 
 public class DualListConfig {
@@ -59,14 +63,26 @@ public class DualListConfig {
     private String selectedListText = "Selected:";
 
     /**
-     * Width of the list controls.
+     * Width of the list controls in pixels.
      */
+    @Deprecated
     private int listWidth = 100;
 
     /**
-     * Height of the list controls.
+     * Height of the list controls in pixels.
      */
+    @Deprecated
     private int listHeight = 125;
+
+    /**
+     * Width of the list controls in characters.
+     */
+    private int listWidthInChars = SWT.DEFAULT;
+
+    /**
+     * Height of the list controls in items.
+     */
+    private int visibleItems = SWT.DEFAULT;
 
     /**
      * Flag to determine if the up/down buttons should be shown.
@@ -87,7 +103,7 @@ public class DualListConfig {
      * The include list is a set of items that will always be present the
      * selected list and cannot be removed from the selected list.
      */
-    private HashSet<String> includeList = new HashSet<>();
+    private Set<String> includeList = new HashSet<>();
 
     /**
      * The search field.
@@ -128,26 +144,26 @@ public class DualListConfig {
 
     /**
      * Get the include list.
-     * 
+     *
      * @return the include list.
      */
-    public HashSet<String> getIncludeList() {
+    public Set<String> getIncludeList() {
         return includeList;
     }
 
     /**
      * Set the include list.
-     * 
+     *
      * @param includeList
      *            List to always include.
      */
-    public void setIncludeList(HashSet<String> includeList) {
+    public void setIncludeList(Set<String> includeList) {
         this.includeList = includeList;
     }
 
     /**
      * Get the available list header text.
-     * 
+     *
      * @return Available list header text.
      */
     public String getAvailableListText() {
@@ -156,7 +172,7 @@ public class DualListConfig {
 
     /**
      * Set the available list header text.
-     * 
+     *
      * @param availableListLabel
      *            Available list header text.
      */
@@ -166,7 +182,7 @@ public class DualListConfig {
 
     /**
      * Get the selected list header text.
-     * 
+     *
      * @return Selected list header text.
      */
     public String getSelectedListText() {
@@ -175,7 +191,7 @@ public class DualListConfig {
 
     /**
      * Set the selected list header text.
-     * 
+     *
      * @param selectedListLabel
      *            Selected list header text.
      */
@@ -185,45 +201,97 @@ public class DualListConfig {
 
     /**
      * Get the list control width.
-     * 
+     *
      * @return The list width.
+     *
+     * @deprecated use {@link #getListWidthInChars()}
      */
+    @Deprecated
     public int getListWidth() {
         return listWidth;
     }
 
     /**
      * Set the width of the list control.
-     * 
+     *
      * @param listWidth
      *            Width of the list control.
+     *
+     * @deprecated use {@link #setListWidthInChars(int)}
      */
+    @Deprecated
     public void setListWidth(int listWidth) {
         this.listWidth = listWidth;
     }
 
     /**
      * Get the height of the list control.
-     * 
+     *
      * @return The height of the list control.
+     *
+     * @deprecated use {@link #getVisibleItems()}
      */
+    @Deprecated
     public int getListHeight() {
         return listHeight;
     }
 
     /**
      * Set the height of the list control.
-     * 
+     *
      * @param listHeight
      *            The height of the list control.
+     *
+     * @deprecated use {@link #setVisibleItems(int)}
      */
+    @Deprecated
     public void setListHeight(int listHeight) {
         this.listHeight = listHeight;
     }
 
     /**
+     * Get the list control width.
+     *
+     * @return The list width in characters.
+     */
+    public int getListWidthInChars() {
+        return listWidthInChars;
+    }
+
+    /**
+     * Set the width of the list control.
+     *
+     * @param listWidthInChars
+     *            Width of the list control in characters.
+     */
+    public void setListWidthInChars(int listWidthInChars) {
+        this.listWidthInChars = listWidthInChars;
+        this.listWidth = SWT.DEFAULT;
+    }
+
+    /**
+     * Get the number of items to be visible in the list.
+     *
+     * @return number of visible list items.
+     */
+    public int getVisibleItems() {
+        return visibleItems;
+    }
+
+    /**
+     * Set the number of items to be visible in the list.
+     *
+     * @param visibleItems
+     *            The number of visible list items.
+     */
+    public void setVisibleItems(int visibleItems) {
+        this.visibleItems = visibleItems;
+        this.listHeight = SWT.DEFAULT;
+    }
+
+    /**
      * Check if the up/down buttons should be shown.
-     * 
+     *
      * @return True if the buttons are shown, false if hidden.
      */
     public boolean isShowUpDownBtns() {
@@ -232,7 +300,7 @@ public class DualListConfig {
 
     /**
      * Set the show up/down button flag.
-     * 
+     *
      * @param showUpDownBtns
      *            True to show the buttons, false to not show the buttons.
      */
@@ -242,7 +310,7 @@ public class DualListConfig {
 
     /**
      * Get an array of selected items.
-     * 
+     *
      * @return An array of selected items.
      */
     public List<String> getSelectedList() {
@@ -251,7 +319,7 @@ public class DualListConfig {
 
     /**
      * Set the array of selected items.
-     * 
+     *
      * @param selectedList
      *            Array of selected items.
      */
@@ -261,7 +329,7 @@ public class DualListConfig {
 
     /**
      * Get an array of all of the available items.
-     * 
+     *
      * @return The array of all available items.
      */
     public List<String> getFullList() {
@@ -270,7 +338,7 @@ public class DualListConfig {
 
     /**
      * Set the array of all of the available items.
-     * 
+     *
      * @param fullList
      *            The array of all available items.
      */
@@ -280,7 +348,7 @@ public class DualListConfig {
 
     /**
      * Get the search field text.
-     * 
+     *
      * @return the String the search field text.
      */
     public String getSearchField() {
@@ -289,7 +357,7 @@ public class DualListConfig {
 
     /**
      * Set the search field text.
-     * 
+     *
      * @param searchField
      *            the search field text.
      */
@@ -352,7 +420,7 @@ public class DualListConfig {
 
     /**
      * true is match any, false is match all
-     * 
+     *
      * @return the matchAny
      */
     public boolean getMatchAny() {
@@ -361,7 +429,7 @@ public class DualListConfig {
 
     /**
      * true is match any, false is match all
-     * 
+     *
      * @param matchAny
      *            the matchAny to set
      */
@@ -371,7 +439,7 @@ public class DualListConfig {
 
     /**
      * When true the dual lists should be sorted. Default is false.
-     * 
+     *
      * @return sortList
      */
     public boolean isSortList() {
@@ -380,7 +448,7 @@ public class DualListConfig {
 
     /**
      * Set the sort state for the dual lists.
-     * 
+     *
      * @param sortList
      */
     public void setSortList(boolean sortList) {
