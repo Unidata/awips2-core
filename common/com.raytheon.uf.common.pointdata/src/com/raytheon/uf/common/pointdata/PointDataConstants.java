a16226c8b5855ee4a6f6430a86618e3abd88a706
Tom Gurney <tom.gurney@raytheon.com>
2017-04-10 09:40:21 -0500

    Omaha #6110 Add LOCATION_STATIONID to PointDataConstants
    Change-Id: Id63b44bbb3268d4c2625d991f5f1a42ceaffed30

--- common/com.raytheon.uf.common.pointdata/src/com/raytheon/uf/common/pointdata/PointDataConstants.java
+++ common/com.raytheon.uf.common.pointdata/src/com/raytheon/uf/common/pointdata/PointDataConstants.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -21,18 +21,18 @@ package com.raytheon.uf.common.pointdata;
 
 /**
  * Constants used in pointdata
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Apr 14, 2009            chammack     Initial creation
- * 
+ * Apr 14, 2009            chammack    Initial creation
+ * Apr 10, 2017      6110  tgurney     Add LOCATION_STATIONID
+ *
  * </pre>
- * 
+ *
  * @author chammack
- * @version 1.0
  */
 
 public class PointDataConstants {
@@ -46,5 +46,7 @@ public class PointDataConstants {
     public static final String DATASET_REFTIME = "refTime";
 
     public static final String DATASET_FORECASTHR = "forecastHr";
+
+    public static final String LOCATION_STATIONID = "location.stationId";
 
 }
