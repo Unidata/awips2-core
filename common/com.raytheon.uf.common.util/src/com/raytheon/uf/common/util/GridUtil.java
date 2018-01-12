047a12a92b72e9ff34a4bb87638d895d99e68e81
Ron Anderson <ron.anderson@raytheon.com>
2018-01-12 11:23:57 -0600

    Omaha #7178 Code cleanup
    Change-Id: I262c52a01f0e51400f8537e2fabde049b8e55a56

--- common/com.raytheon.uf.common.util/src/com/raytheon/uf/common/util/GridUtil.java
+++ common/com.raytheon.uf.common.util/src/com/raytheon/uf/common/util/GridUtil.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -21,28 +21,29 @@ package com.raytheon.uf.common.util;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Utilities for grids and images
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
+ *
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Jan 26, 2011            njensen     Initial creation
  * Feb 15, 2013 1638       mschenke    Moved GRID_FILL_VALUE from edex.common Util
- * 
+ * Jan 09, 2018 7178       randerso    Code cleanup
+ *
  * </pre>
- * 
+ *
  * @author njensen
  * @version 1.0
  */
 
 public class GridUtil {
-    
-    
+
     /**
      * Moved from edex common util to clean up dependencies. Still need to
      * figure out method for handling fill values and update
@@ -54,7 +55,7 @@ public class GridUtil {
      * This version is designed to add points to an existing ArrayList. The
      * first point is not added so that this routine can be called while walking
      * down a list of points without inserting duplicate points in the list.
-     * 
+     *
      * @param p1
      *            the first point
      * @param p2
@@ -62,89 +63,117 @@ public class GridUtil {
      * @param ptList
      *            list to which resulting points are added
      */
-    public static void bresenham(Point p1, Point p2, ArrayList<Point> ptList) {
-    
+    public static void bresenham(Point p1, Point p2, List<Point> ptList) {
+
         // I wrote this from pseudo code found online explaining the algorithm
-        int deltax = Math.abs(p2.x - p1.x); // The difference between the x's
-        int deltay = Math.abs(p2.y - p1.y); // The difference between the y's
-        int x = p1.x; // Start x off at the first pixel
-        int y = p1.y; // Start y off at the first pixel
-    
+
+        // The difference between the x's
+        int deltax = Math.abs(p2.x - p1.x);
+
+        // The difference between the y's
+        int deltay = Math.abs(p2.y - p1.y);
+
+        // Start x off at the first pixel
+        int x = p1.x;
+
+        // Start y off at the first pixel
+        int y = p1.y;
+
         int xinc1 = 0;
         int xinc2 = 0;
         int yinc1 = 0;
         int yinc2 = 0;
-    
-        if (p2.x >= p1.x) // The x-values are increasing
-        {
+
+        if (p2.x >= p1.x) {
+            // The x-values are increasing
             xinc1 = 1;
             xinc2 = 1;
-        } else // The x-values are decreasing
-        {
+        } else {
+            // The x-values are decreasing
             xinc1 = -1;
             xinc2 = -1;
         }
-    
-        if (p2.y >= p1.y) // The y-values are increasing
-        {
+
+        if (p2.y >= p1.y) {
+            // The y-values are increasing
             yinc1 = 1;
             yinc2 = 1;
-        } else // The y-values are decreasing
-        {
+        } else {
+            // The y-values are decreasing
             yinc1 = -1;
             yinc2 = -1;
         }
-    
+
         int den = 0;
         int num = 0;
         int numadd = 0;
         int numpixels = 0;
-    
-        if (deltax >= deltay) // There is at least one x-value for every
-        // y-value
-        {
-            xinc1 = 0; // Don't change the x when numerator >= denominator
-            yinc2 = 0; // Don't change the y for every iteration
+
+        if (deltax >= deltay) {
+            // There is at least one x-value for every y-value
+
+            // Don't change the x when numerator >= denominator
+            xinc1 = 0;
+
+            // Don't change the y for every iteration
+            yinc2 = 0;
             den = deltax;
             num = deltax / 2;
             numadd = deltay;
-            numpixels = deltax; // There are more x-values than y-values
-        } else // There is at least one y-value for every x-value
-        {
-            xinc2 = 0; // Don't change the x for every iteration
-            yinc1 = 0; // Don't change the y when numerator >= denominator
+
+            // There are more x-values than y-values
+            numpixels = deltax;
+        } else {
+            // There is at least one y-value for every x-value
+
+            // Don't change the x for every iteration
+            xinc2 = 0;
+
+            // Don't change the y when numerator >= denominator
+            yinc1 = 0;
             den = deltay;
             num = deltay / 2;
             numadd = deltax;
-            numpixels = deltay; // There are more y-values than x-values
+
+            // There are more y-values than x-values
+            numpixels = deltay;
         }
-    
+
         for (int i = 0; i < numpixels; i++) {
-            // PutPixel(x, y); // Draw the current pixel
-            num += numadd; // Increase the numerator by the top of the fraction
-            if (num >= den) // Check if numerator >= denominator
-            {
-                num -= den; // Calculate the new numerator value
-                x += xinc1; // Change the x as appropriate
-                y += yinc1; // Change the y as appropriate
+            // Increase the numerator by the top of the fraction
+            num += numadd;
+
+            // Check if numerator >= denominator
+            if (num >= den) {
+                // Calculate the new numerator value
+                num -= den;
+
+                // Change the x as appropriate
+                x += xinc1;
+
+                // Change the y as appropriate
+                y += yinc1;
             }
-            x += xinc2; // Change the x as appropriate
-            y += yinc2; // Change the y as appropriate
+
+            // Change the x as appropriate
+            x += xinc2;
+            // Change the y as appropriate
+            y += yinc2;
             ptList.add(new Point(x, y));
         }
     }
 
     /**
      * Bresenham's algorithm to determine closest points along a line on a grid
-     * 
+     *
      * @param p1
      *            the first point
      * @param p2
      *            the second point
      * @return the closest points along the line
      */
-    public static ArrayList<Point> bresenham(Point p1, Point p2) {
-        ArrayList<Point> ptList = new ArrayList<Point>();
+    public static List<Point> bresenham(Point p1, Point p2) {
+        List<Point> ptList = new ArrayList<>();
         ptList.add(p1);
         bresenham(p1, p2, ptList);
         return ptList;
