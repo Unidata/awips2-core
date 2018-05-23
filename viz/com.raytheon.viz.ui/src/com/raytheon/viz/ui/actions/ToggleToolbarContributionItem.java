740fdc780a1ba5cfa1b1f9e50dec9667dbe1d585
Tom Gurney <tom.gurney@raytheon.com>
2018-05-23 15:37:07 -0500

    Omaha #7298 Add Toolbar toggle to the global CAVE menu
    Change-Id: I2f2a5420324d7d217e7038d9f040c5d077b07c37

--- viz/com.raytheon.viz.ui/src/com/raytheon/viz/ui/actions/ToggleToolbarContributionItem.java
+++ viz/com.raytheon.viz.ui/src/com/raytheon/viz/ui/actions/ToggleToolbarContributionItem.java
@@ -1,3 +1,22 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ *
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ *
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ *
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.viz.ui.actions;
 
 import org.eclipse.core.expressions.IEvaluationContext;
@@ -13,21 +32,37 @@ import org.eclipse.ui.services.IEvaluationService;
 
 import com.raytheon.viz.ui.VizWorkbenchManager;
 
+/**
+ * Menu item to toggle the global toolbar (the one at the top of the screen on
+ * all perspectives)
+ *
+ * <pre>
+ *
+ * SOFTWARE HISTORY
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * May 23, 2018 7298       tgurney     Rename menu item from "Toolbar" to
+ *                                     "Show Toolbar"
+ * </pre>
+ *
+ * @author unknown
+ */
+
 public class ToggleToolbarContributionItem extends CompoundContributionItem {
 
     @Override
     protected IContributionItem[] getContributionItems() {
         ActionContributionItem toggleContributionItem = new ActionContributionItem(
-                new Action("Toolbar", IAction.AS_CHECK_BOX) {
+                new Action("Show Toolbar", IAction.AS_CHECK_BOX) {
                     @Override
                     public boolean isChecked() {
                         IWorkbenchWindow activeWorkbenchWindow = VizWorkbenchManager
                                 .getInstance().getCurrentWindow();
-                        IEvaluationService service = (IEvaluationService) activeWorkbenchWindow
+                        IEvaluationService service = activeWorkbenchWindow
                                 .getService(IEvaluationService.class);
                         IEvaluationContext appState = service.getCurrentState();
-                        Boolean visible = (Boolean) appState
-                                .getVariable(ISources.ACTIVE_WORKBENCH_WINDOW_IS_COOLBAR_VISIBLE_NAME);
+                        Boolean visible = (Boolean) appState.getVariable(
+                                ISources.ACTIVE_WORKBENCH_WINDOW_IS_COOLBAR_VISIBLE_NAME);
                         return (visible != null ? visible : false);
                     }
 
@@ -35,10 +70,6 @@ public class ToggleToolbarContributionItem extends CompoundContributionItem {
                     public void run() {
                         IWorkbenchWindow activeWorkbenchWindow = VizWorkbenchManager
                                 .getInstance().getCurrentWindow();
-                        // ActionFactory.IWorkbenchAction toggleToolbar =
-                        // ActionFactory.TOGGLE_COOLBAR
-                        // .create(activeWorkbenchWindow);
-                        // toggleToolbar.run();
 
                         if (activeWorkbenchWindow instanceof WorkbenchWindow) {
                             WorkbenchWindow window = (WorkbenchWindow) activeWorkbenchWindow;
