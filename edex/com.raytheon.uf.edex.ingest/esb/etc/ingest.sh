889fe855c1ab8ed914300bee7f2087210c263a03
mjames-upc <mjames@unidata.ucar.edu>
2018-07-13 15:13:51 -0600

    ingest (4GB) and request (2GB) explicit max mem

--- edex/com.raytheon.uf.edex.ingest/esb/etc/ingest.sh
+++ edex/com.raytheon.uf.edex.ingest/esb/etc/ingest.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 ##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
-##
-
-##
 # The ingest mode was originally designed to handle decoding, storage, and
 # purge of data received from the SBN.  Over time it took on the role of
 # running all services not covered by the request, ingestGrib, and ingestDat
 # JVMs.
 ##
-
-export MAX_MEM=1536 # in Meg
-
-if [ $HIGH_MEM == "on" ]; then
-    export MAX_MEM=$((MAX_MEM*2))
-fi
-
+export INIT_MEM=512 # in Meg
+export MAX_MEM=4096 # in Meg
 export EDEX_DEBUG_PORT=5006
--- edex/com.raytheon.uf.edex.requestsrv/esb/etc/request.sh
+++ edex/com.raytheon.uf.edex.requestsrv/esb/etc/request.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 ##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
-##
-
-##
 # The request mode was designed to handle all requests from CAVE.  Therefore
 # if services on another JVM failed, CAVE could still receive and send data.
 # A few other services have crept in but this JVM mode remains relatively
@@ -28,17 +8,12 @@
 # The creation of the Java class RequestRouter made it possible for CAVE code to
 # easily send requests to other JVMs.
 ##
+export INIT_MEM=128 # in Meg
+export MAX_MEM=2024 # in Meg
 
-export MAX_MEM=2304 # in Meg
-
-if [ $HIGH_MEM == "on" ]; then
-    export MAX_MEM=4096
-fi
-
-export SERIALIZE_POOL_MAX_SIZE=24
+export SERIALIZE_POOL_MAX_SIZE=500
 export SERIALIZE_STREAM_INIT_SIZE_MB=2
 export SERIALIZE_STREAM_MAX_SIZE_MB=8
-
 
 export EDEX_DEBUG_PORT=5005
 export HTTP_PORT=9581
