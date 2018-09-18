09d8f9d0ac8061e57b74e9a65642aca1b2f15761
Ben Steffensmeier <Ben.Steffensmeier@raytheon.com>
2018-09-18 13:24:43 -0500

    Omaha #7443 New editors will be similar to existing editors.

--- viz/com.raytheon.viz.ui/src/com/raytheon/viz/ui/actions/NewAbstractEditor.java
+++ viz/com.raytheon.viz.ui/src/com/raytheon/viz/ui/actions/NewAbstractEditor.java
@@ -26,7 +26,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
@@ -35,7 +35,6 @@ import com.raytheon.uf.viz.core.IDisplayPane;
 import com.raytheon.uf.viz.core.drawables.IRenderableDisplay;
 import com.raytheon.uf.viz.core.exception.VizException;
 import com.raytheon.viz.ui.UiUtil;
-import com.raytheon.viz.ui.VizWorkbenchManager;
 import com.raytheon.viz.ui.editor.AbstractEditor;
 import com.raytheon.viz.ui.perspectives.AbstractVizPerspectiveManager;
 import com.raytheon.viz.ui.perspectives.VizPerspectiveListener;
@@ -47,48 +46,29 @@ import com.raytheon.viz.ui.perspectives.VizPerspectiveListener;
  * 
  * SOFTWARE HISTORY
  * 
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Oct 25, 2010            mschenke     Initial creation
- * Mar 19, 2013    1808   njensen      Perspective specific behavior takes priority
+ * Date          Ticket#  Engineer  Description
+ * ------------- -------- --------- --------------------------------------------
+ * Oct 25, 2010           mschenke  Initial creation
+ * Mar 19, 2013  1808     njensen   Perspective specific behavior takes priority
+ * Sep 18, 2018  7443     bsteffen  Revert previous change.
  * 
  * </pre>
  * 
  * @author mschenke
- * @version 1.0
  */
 
 public class NewAbstractEditor extends AbstractHandler {
     private static final transient IUFStatusHandler statusHandler = UFStatus
             .getHandler(NewAbstractEditor.class);
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
-     * ExecutionEvent)
-     */
     @Override
     public AbstractEditor execute(ExecutionEvent event)
             throws ExecutionException {
-        // allow perspective specific behavior first if available
-        IWorkbenchWindow window = VizWorkbenchManager.getInstance()
-                .getCurrentWindow();
-        AbstractVizPerspectiveManager mgr = VizPerspectiveListener.getInstance(
-                window).getActivePerspectiveManager();
-        if (mgr != null) {
-            AbstractEditor editor = mgr.openNewEditor();
-            if (editor != null) {
-                return editor;
-            }
-        }
 
-        IEditorPart part = VizWorkbenchManager.getInstance().getActiveEditor(
-                window);
+        IEditorPart part = HandlerUtil.getActiveEditor(event);
         if (part instanceof AbstractEditor) {
             AbstractEditor editor = (AbstractEditor) part;
-            List<IRenderableDisplay> displays = new ArrayList<IRenderableDisplay>();
+            List<IRenderableDisplay> displays = new ArrayList<>();
             for (IDisplayPane pane : editor.getDisplayPanes()) {
                 IRenderableDisplay toClone = pane.getRenderableDisplay();
                 IRenderableDisplay newDisplay = toClone.createNewDisplay();
@@ -102,18 +82,24 @@ public class NewAbstractEditor extends AbstractHandler {
                 // Reset extents on renderable displays when getting new editor
                 for (IDisplayPane pane : ae.getDisplayPanes()) {
                     pane.getRenderableDisplay().getView().getExtent().reset();
-                    pane.getRenderableDisplay().scaleToClientArea(
-                            pane.getBounds());
+                    pane.getRenderableDisplay()
+                            .scaleToClientArea(pane.getBounds());
                 }
             }
             return ae;
         } else {
+            AbstractVizPerspectiveManager mgr = VizPerspectiveListener
+                    .getCurrentPerspectiveManager();
             String msg = "Opening new empty editor not supported by perspective";
             if (mgr != null) {
+                AbstractEditor ae = mgr.openNewEditor();
+                if (ae != null) {
+                    return ae;
+                }
                 msg += ": " + mgr.getPerspectiveId();
             }
-            statusHandler.handle(Priority.PROBLEM, msg, new VizException(
-                    "Operation not supported"));
+            statusHandler.handle(Priority.PROBLEM, msg,
+                    new VizException("Operation not supported"));
         }
         return null;
     }
