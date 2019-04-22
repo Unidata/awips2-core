ef5690b6b5c64bf78628c5272976e6901ae5bc3b
David Gillingham <David.Gillingham@raytheon.com>
2019-04-22 15:54:53 -0500

    Omaha #7522 - Fix qpid-crashing bug caused by previous commit.
    Change-Id: I1ce3ef1f8316e0eb30786bf26d8b614ac0b1423d

--- edex/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/FileToString.java
+++ edex/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/FileToString.java
@@ -20,10 +20,26 @@
 package com.raytheon.uf.edex.esb.camel;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.EnumSet;
+import java.util.Set;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.component.file.GenericFile;
+
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.util.FileUtil;
+import com.raytheon.uf.common.util.file.Files;
+import com.raytheon.uf.common.util.file.IOPermissionsHelper;
 
 /**
  * Provides a capability to transform java.io.File to Strings. This is necessary
@@ -39,7 +55,8 @@ import org.apache.camel.component.file.GenericFile;
  * ------------ ---------- ----------- --------------------------
  * Dec 03, 2008            chammack    Initial creation
  * Jun 12, 2012 00609      djohnson    Use EDEXUtil for EDEX_HOME.
- * Oct 16, 2018 #7522      dgilling    Stop moving file.
+ * Oct 16, 2018 #7522      dgilling    Move file to
+ *                                     ${data.archive.root}/manual/edex_processing.
  *
  * </pre>
  *
@@ -49,10 +66,90 @@ import org.apache.camel.component.file.GenericFile;
 @Deprecated
 public class FileToString implements Processor {
 
+    private final IUFStatusHandler statusHandler = UFStatus
+            .getHandler(getClass());
+
+    private static final String DIR = System.getProperty("data.archive.root")
+            + File.separator + "manual" + File.separator + "edex_processing";
+
+    private static final Set<PosixFilePermission> POSIX_FILE_PERMISSIONS = EnumSet
+            .of(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.GROUP_READ,
+                    PosixFilePermission.GROUP_WRITE);
+
+    private static final FileAttribute<Set<PosixFilePermission>> POSIX_DIRECTORY_ATTRIBUTES = PosixFilePermissions
+            .asFileAttribute(EnumSet.of(PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.OWNER_EXECUTE,
+                    PosixFilePermission.GROUP_READ,
+                    PosixFilePermission.GROUP_WRITE,
+                    PosixFilePermission.GROUP_EXECUTE));
+
+    private static final DateTimeFormatter SUB_DIR_FORMATTER = DateTimeFormatter
+            .ofPattern("yyyyMMdd" + File.separatorChar + "HH")
+            .withZone(ZoneOffset.UTC);
+
     @Override
     public void process(Exchange arg0) throws Exception {
         File file = (File) ((GenericFile<?>) arg0.getIn().getBody()).getFile();
-        arg0.getIn().setBody(file.toString());
-        arg0.getIn().setHeader("enqueueTime", System.currentTimeMillis());
+        File newFile = moveFileToDataStore(file);
+        if (newFile != null) {
+            arg0.getIn().setBody(newFile.toString());
+            arg0.getIn().setHeader("enqueueTime", System.currentTimeMillis());
+        } else {
+            arg0.getOut().setFault(true);
+        }
+    }
+
+    public File moveFileToDataStore(File inFile) throws IOException {
+        if (!inFile.exists()) {
+            statusHandler
+                    .warn("Attempting to process non-existent file: " + inFile);
+            return null;
+        }
+
+        String dateTimeString = ZonedDateTime.now(ZoneOffset.UTC)
+                .format(SUB_DIR_FORMATTER);
+        String path = FileUtil.join(DIR, dateTimeString);
+        File dir = new File(path);
+        File newFile = new File(dir, inFile.getName());
+
+        try {
+            if (!dir.exists()) {
+                Files.createDirectories(dir.toPath(),
+                        POSIX_DIRECTORY_ATTRIBUTES);
+            }
+
+            java.nio.file.Files.move(inFile.toPath(), newFile.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            /*
+             * Attempt to adjust the file permissions to fulfill the security
+             * requirements. As of May 2017, all of the files that will be
+             * processed are provided by an external source.
+             */
+            try {
+                IOPermissionsHelper.applyFilePermissions(newFile.toPath(),
+                        POSIX_FILE_PERMISSIONS);
+            } catch (Exception e) {
+                /*
+                 * Permission updates have failed. However, we still probably
+                 * want to keep the file so that it can successfully be ingested
+                 * and used?
+                 */
+                statusHandler.warn(e.getMessage(), e);
+            }
+
+            statusHandler.info("edex_processing: " + inFile.getAbsolutePath());
+        } catch (IOException e) {
+            statusHandler.error(
+                    "Failed to move file [" + inFile.getAbsolutePath()
+                            + "] to edex_processing dir. File will be discarded.",
+                            e);
+            inFile.delete();
+            return null;
+        }
+
+        return newFile;
     }
 }
