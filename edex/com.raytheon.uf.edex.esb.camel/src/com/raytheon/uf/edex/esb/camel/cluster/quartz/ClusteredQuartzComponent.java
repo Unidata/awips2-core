26e1b5e8da1a7dad2ab0d498ab89f23c43728dab
Tom Gurney <tom.gurney@raytheon.com>
2019-04-25 12:03:20 -0500

    Omaha #7715 Roll back the Quartz 2 upgrade
    Upgrading to Quartz 2 is a large work effort that is not yet necessary.
    
    Change-Id: Ieb40a1fc2eee695d97c4dba39dd7b8b0bb2e95f7

--- edex/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/cluster/quartz/ClusteredQuartzComponent.java
+++ edex/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/cluster/quartz/ClusteredQuartzComponent.java
@@ -1,52 +1,47 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
 package com.raytheon.uf.edex.esb.camel.cluster.quartz;
 
 import java.net.URI;
-
-
 import java.util.Date;
 import java.util.Map;
 
 import org.apache.camel.component.quartz.QuartzComponent;
 import org.apache.camel.util.IntrospectionSupport;
 import org.apache.camel.util.ObjectHelper;
-import org.quartz.CronScheduleBuilder;
+import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
 
 /**
  * TODO Add Description
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Feb 22, 2010            njensen     Initial creation
- * Apr  8, 2019            skabasele   updated org.quartz.Trigger to version 2.3.0
- * 
+ *
  * </pre>
- * 
+ *
  * @author njensen
- * @version 1.0
  */
 
 public class ClusteredQuartzComponent extends QuartzComponent {
@@ -83,30 +78,24 @@ public class ClusteredQuartzComponent extends QuartzComponent {
 
         // create the trigger either cron or simple
         Trigger trigger;
-        TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger()
-                .withIdentity(name, group);
-
         if (ObjectHelper.isNotEmpty(cron)) {
-
-            trigger = triggerBuilder
-                    .withSchedule(CronScheduleBuilder.cronSchedule(cron))
-                    .build();
-
+            trigger = createCronTrigger(cron);
         } else {
-
+            trigger = new SimpleTrigger();
             if (fireNow) {
                 String intervalString = (String) triggerParameters
                         .get("repeatInterval");
                 if (intervalString != null) {
                     long interval = Long.valueOf(intervalString);
-                    triggerBuilder.startAt(
+                    trigger.setStartTime(
                             new Date(System.currentTimeMillis() - interval));
-
                 }
             }
-            trigger = triggerBuilder.build();
         }
         answer.setTrigger(trigger);
+
+        trigger.setName(name);
+        trigger.setGroup(group);
 
         setProperties(trigger, triggerParameters);
         setProperties(answer.getJobDetail(), jobParameters);
