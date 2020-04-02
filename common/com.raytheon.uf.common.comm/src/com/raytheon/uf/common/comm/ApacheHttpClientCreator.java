0f686d353104d33a200e2f9fabd3e2a072d77b7d
Ron Anderson <ron.anderson@raytheon.com>
2020-04-02 18:12:59 -0500

    Omaha #8086 Use HttpClientBuilder.useSystemProperties() to handle proxy settings
    Change-Id: I89d94f4097dce40aa78d318b85007fa79d032ded

--- common/com.raytheon.uf.common.comm/src/com/raytheon/uf/common/comm/ApacheHttpClientCreator.java
+++ common/com.raytheon.uf.common.comm/src/com/raytheon/uf/common/comm/ApacheHttpClientCreator.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -29,7 +29,6 @@ import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 
-import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
@@ -51,27 +50,31 @@ import com.raytheon.uf.common.util.app.AppInfo;
 
 /**
  * Utility for constructing apache http client instances
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Sep 04, 2014  3570      bclement     Initial creation
- * Nov 15, 2014  3757      dhladky      Added general certificate checks.
- * Jan 22, 2015  3952      njensen      Removed gzip handling as apache http client has it built-in
- * May 10, 2015  4435      dhladky      PDA necessitated the loading of keyMaterial as well as 
- *                                       trustMaterial.
- * Jul 01, 2015  4021      bsteffen     Set User Agent
- * Jul 06, 2015  4614      njensen      Disable gzip by default
- * Dec 07, 2015  4834      njensen      Changes for rename of IHttpsHandler to HttpAuthHandler
- * Jan 27, 2016  5170      tjensen      Removed log interceptors. Logging moved to methods where 
- *                                       message type is known.
- * Jan 31, 2017  6083      bsteffen     Remove local trust strategy
- * 
+ *
+ * Date          Ticket#  Engineer  Description
+ * ------------- -------- --------- --------------------------------------------
+ * Sep 04, 2014  3570     bclement  Initial creation
+ * Nov 15, 2014  3757     dhladky   Added general certificate checks.
+ * Jan 22, 2015  3952     njensen   Removed gzip handling as apache http client
+ *                                  has it built-in
+ * May 10, 2015  4435     dhladky   PDA necessitated the loading of keyMaterial
+ *                                  as well as trustMaterial.
+ * Jul 01, 2015  4021     bsteffen  Set User Agent
+ * Jul 06, 2015  4614     njensen   Disable gzip by default
+ * Dec 07, 2015  4834     njensen   Changes for rename of IHttpsHandler to
+ *                                  HttpAuthHandler
+ * Jan 27, 2016  5170     tjensen   Removed log interceptors. Logging moved to
+ *                                  methods where message type is known.
+ * Jan 31, 2017  6083     bsteffen  Remove local trust strategy
+ * Apr 02, 2020  8086     randerso  Use HttpClientBuilder.useSystemProperties() to
+ *                                  handle proxy settings
+ *
  * </pre>
- * 
+ *
  * @author bclement
  */
 public class ApacheHttpClientCreator {
@@ -99,7 +102,7 @@ public class ApacheHttpClientCreator {
 
     /**
      * Construct a new HTTP client that is configured for HTTPS connections
-     * 
+     *
      * @param config
      * @param stats
      * @return an https client based on the config
@@ -110,8 +113,8 @@ public class ApacheHttpClientCreator {
      */
     public static CloseableHttpClient createSslClient(HttpClientConfig config,
             NetworkStatistics stats) throws NoSuchAlgorithmException,
-            KeyStoreException, KeyManagementException,
-            UnrecoverableKeyException {
+    KeyStoreException, KeyManagementException,
+    UnrecoverableKeyException {
 
         SSLContextBuilder sslCtxBuilder = new SSLContextBuilder();
         HttpAuthHandler handler = config.getHttpAuthHandler();
@@ -140,8 +143,8 @@ public class ApacheHttpClientCreator {
                 sslCtxBuilder.loadKeyMaterial(keystore,
                         handler.getKeystorePassword());
                 statusHandler
-                        .handle(Priority.DEBUG,
-                                "Proceeding with validation of certificates.  Presenting key(s) for validation.");
+                .handle(Priority.DEBUG,
+                        "Proceeding with validation of certificates.  Presenting key(s) for validation.");
 
             } else {
                 /*
@@ -150,8 +153,8 @@ public class ApacheHttpClientCreator {
                  * clients.
                  */
                 statusHandler
-                        .handle(Priority.DEBUG,
-                                "Proceeding with validation of certificates.  Not presenting key(s) for validation.");
+                .handle(Priority.DEBUG,
+                        "Proceeding with validation of certificates.  Not presenting key(s) for validation.");
             }
 
         } else {
@@ -170,8 +173,8 @@ public class ApacheHttpClientCreator {
 
             // Do no validation what so ever
             statusHandler
-                    .handle(Priority.DEBUG,
-                            "Proceeding with no validation of certificates or key submission.");
+            .handle(Priority.DEBUG,
+                    "Proceeding with no validation of certificates or key submission.");
         }
 
         SSLContext sslCtx = sslCtxBuilder.build();
@@ -189,13 +192,8 @@ public class ApacheHttpClientCreator {
         RequestConfig.Builder reqConfigBuilder = RequestConfig.custom();
         SocketConfig.Builder soConfigBuilder = SocketConfig.custom();
 
-        // Set the proxy info
-        if (ProxyConfiguration.HTTPS_PROXY_DEFINED) {
-            HttpHost proxy = new HttpHost(
-                    ProxyConfiguration.getHttpsProxyHost(),
-                    ProxyConfiguration.getHttpsProxyPort());
-            reqConfigBuilder.setProxy(proxy);
-        }
+        // Use system properties to get the proxy info
+        clientBuilder.useSystemProperties();
 
         reqConfigBuilder.setSocketTimeout(config.getSocketTimeout());
         soConfigBuilder.setSoTimeout(config.getSocketTimeout());
@@ -234,7 +232,7 @@ public class ApacheHttpClientCreator {
 
     /**
      * Creates a new HTTP client according to the provided configuration
-     * 
+     *
      * @param config
      * @param stats
      * @return an http client based on the config
