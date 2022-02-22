f264cd3bd15929d147c38def99e8a93bca9005b9
Ron Anderson <ron.anderson@raytheon.com>
2022-02-22 11:08:33 -0600

    Omaha #8788 Replace deprecated rank() with ndim()
    Change-Id: I44d5ee05994b42c1823db831e8f65971eabce3d2

--- common/com.raytheon.uf.common.derivparam.python/utility/common_static/base/derivedParameters/functions/TW.py
+++ common/com.raytheon.uf.common.derivparam.python/utility/common_static/base/derivedParameters/functions/TW.py
@@ -17,14 +17,14 @@
 # See the AWIPS II Master Rights File ("Master Rights File.pdf") for
 # further licensing information.
 ##
-
 #
 # SOFTWARE HISTORY
 #
-# Date           Ticket#      Engineer      Description
-# ------------   ----------   -----------   -----------
-#                             ????          Initial creation
-# Aug 05, 2015   4703         njensen       cast -999999.0 to float32
+# Date          Ticket#  Engineer  Description
+# ------------- -------- --------- --------------------------------------------
+#                        ????      Initial creation
+# Aug 05, 2015  4703     njensen   cast -999999.0 to float32
+# Feb 22, 2022  ????     randerso  Replace deprecated rank() with ndim()
 #
 
 ## @file TW.py
@@ -35,7 +35,7 @@ from numpy import clip
 from numpy import empty
 from numpy import exp
 from numpy import log
-from numpy import rank
+from numpy import ndim
 from numpy import sqrt
 from numpy import zeros
 from numpy import where
@@ -48,6 +48,7 @@ c1 = 0.0091379024
 ## @var c2: constant used in multiple functions.
 c2 = 6106.3960
 
+
 ##
 # Calculate wet bulb temperature in degrees C
 # from pressure, temperature, and relative humidity.
@@ -55,22 +56,23 @@ c2 = 6106.3960
 # @param P: Pressure in millibars
 # @param T: Temperature in degrees Kelvin
 # @param RH: Relative humidity from 0 to 100
-# @return: wet bulb temperature in degrees C as a numpy array 
+# @return: wet bulb temperature in degrees C as a numpy array
 #
 def execute1(P, T, RH):
 
     # Find dry-bulb temp from temp and relative humidity
-    RH = clip(RH,1.0,100.0)
+    RH = clip(RH, 1.0, 100.0)
     b = T * c1
-    b += c2/T
-    b -= log(RH/100.0)
-    val = b*b
+    b += c2 / T
+    b -= log(RH / 100.0)
+    val = b * b
     val -= 223.1986
     td = b - sqrt(val)
     td /= 0.0182758048
 
     TW = myTW(T, td, P)
     return TW
+
 
 def execute2(GH3D, T3D, RH3D, TILT):
     P = zeros(T3D[0][0].shape, 'float32')
@@ -85,6 +87,7 @@ def execute2(GH3D, T3D, RH3D, TILT):
     slice = gridslice.createNumpySlice(vc3d, slice3d, TILT, 2)
     slice = where(slice == 9.99999993e+36, float32(-999999.0), slice)
     return slice
+
 
 ##
 # This function takes arrays of values and filters them against expected values
@@ -101,7 +104,7 @@ def execute2(GH3D, T3D, RH3D, TILT):
 # @rtype: assumed to be a numpy array.
 def myTW(T, Td, P):
     td_lt_t = Td < T
-    t_ge_cold = T>=100.0
+    t_ge_cold = T >= 100.0
 
     typical = td_lt_t & t_ge_cold
 
@@ -112,38 +115,40 @@ def myTW(T, Td, P):
     sane = typical & norm_svp
 
     veryCold = td_lt_t & ~t_ge_cold
-    td_satvp_bad = ~( td_lt_t & norm_svp ) # includes NaN cells
+    td_satvp_bad = ~(td_lt_t & norm_svp)  # includes NaN cells
 
-    if rank(P)==0:
-        sel = lambda val, mask : val
+    if ndim(P) == 0:
+        sel = lambda val, mask: val
     else:
-        sel = lambda val, mask : val[mask]
-        
+        sel = lambda val, mask: val[mask]
+
     TW = empty(T.shape, dtype=T.dtype)
-    TW[td_satvp_bad] = (T[td_satvp_bad] + Td[td_satvp_bad])/2
+    TW[td_satvp_bad] = (T[td_satvp_bad] + Td[td_satvp_bad]) / 2
     TW[veryCold] = T[veryCold]
-    TW[sane] = calcTW(T[sane], Td[sane], sel(P,sane), satVP[sane]) 
-   
+    TW[sane] = calcTW(T[sane], Td[sane], sel(P, sane), satVP[sane])
+
     return(TW)
+
 
 ##
 # Calculate saturation vapor pressure from T.
 #
 # @param T: Temperature in degrees K
 # @return: ew, saturation vapor pressure (in millibars?)
-#    
+#
 def calcSatVP(T):
     "Calculate saturation vapor pressure from T."
     c0 = 26.66082
-    ew = -c1*T
+    ew = -c1 * T
     ew += c0
-    ew -= c2/T
+    ew -= c2 / T
     return ew
 
+
 ##
-# Calculate TW iteratively from the input parameters. This is called 
+# Calculate TW iteratively from the input parameters. This is called
 # from myTW(); the parameter arrays have already been tested for
-# "reasonable" values. 
+# "reasonable" values.
 #
 # @param T: temperature in degrees K
 # @param Td: dry-bulb temperature in degrees K
@@ -158,80 +163,80 @@ def calcTW(T, Td, P, satVP):
     miniscule = 1e-5
     satVPmin = -50.0
     satVPmax = 10.0
-    
+
     # One more special case: "ridiculously small" dewpoint vapor pressure.
     Tdx = Td
     dewptVP = calcSatVP(Tdx)
     dvpLtM50 = dewptVP < satVPmin
-    while( any(dvpLtM50) ):
+    while(any(dvpLtM50)):
         Tdx[dvpLtM50] = Tdx[dvpLtM50] + 10
         dewptVP[dvpLtM50] = calcSatVP(Tdx[dvpLtM50])
         # Update dvpLtM50.
         # The fancy indexing is to skip cells that are already False,
         # since they won't become True as a result of the loop.
         dvpLtM50[dvpLtM50] = dewptVP[dvpLtM50] < satVPmin
-        
+
     fp = P * f
 
-    if rank(fp) == 0:
-        sel = lambda val, mask : val
+    if ndim(fp) == 0:
+        sel = lambda val, mask: val
     else:
-        sel = lambda val, mask : val[mask]
-         
+        sel = lambda val, mask: val[mask]
+
     # we need e**satVP and e**dewptVP for calculating s and the initial TW estimate
     # (e is the base of the natural logarithm).
     expSatVP = exp(satVP)
     expDewptVP = exp(dewptVP)
-    
-    s = expSatVP-expDewptVP
-    s /= T-Tdx
-    TW = T*fp
-    TW += Tdx*s
-    TW /= fp+s
-    
+
+    s = expSatVP - expDewptVP
+    s /= T - Tdx
+    TW = T * fp
+    TW += Tdx * s
+    TW /= fp + s
+
     # new, non-exp'd satVP for the loop below
     satVP = calcSatVP(TW)
 
     # create some boolean arrays to use in the for loop.
     # creating them once here allows us to use the out parameter of
     # the numpy boolean functions to avoid repeatedly allocating space.
-    # satVPinRange = empty(shape(TW), dtype=bool) 
+    # satVPinRange = empty(shape(TW), dtype=bool)
     # svrTemp = empty(shape(TW), dtype=bool)
-    
+
     # At each step of the iteration, esat(Tw)-esat(Td) is compared to
-    # (T-Tw)*p/(eps*L).  When that difference is less than one part in 
+    # (T-Tw)*p/(eps*L).  When that difference is less than one part in
     # 10000 of esat(Tw), or ten iterations have been done, the iteration stops.
     # This is basically trying to find the value of Tw where de is 0.  The
     # value s is the derivative of de with respect to Tw, a fairly standard
     # numerical technique for finding the zero value of a function.
     for iteration in range(10):
-        satVPinRange = (satVPmin<=satVP) & (satVP<=satVPmax)
+        satVPinRange = (satVPmin <= satVP) & (satVP <= satVPmax)
         if not any(satVPinRange):
             break
 
         expSatVP = exp(satVP[satVPinRange])
 
-        de = calcDe(T[satVPinRange], TW[satVPinRange], expSatVP, 
-                    expDewptVP[satVPinRange], sel(fp,satVPinRange))
+        de = calcDe(T[satVPinRange], TW[satVPinRange], expSatVP,
+                    expDewptVP[satVPinRange], sel(fp, satVPinRange))
 
-        mag = abs(de/expSatVP)
-
+        mag = abs(de / expSatVP)
 
         significant = miniscule <= mag
         if any(significant):
             satVPinRange[satVPinRange] = significant
             # now we can mix TW and de to obtain a new estimate for TW:
-            TW[satVPinRange] = reestimateTW( TW[satVPinRange],
+            TW[satVPinRange] = reestimateTW(TW[satVPinRange],
                                      expSatVP[significant],
                                      de[significant],
-                                     sel(fp, satVPinRange) )
+                                     sel(fp, satVPinRange))
             # Get a new satVP to see if the iteration can stop.
             # Only cells with a different TW will have a different satVP.
             satVP[satVPinRange] = calcSatVP(TW[satVPinRange])
         else:
             break
-            
+
     return TW
+
 
 ##
 # Calculate the value of the de variable used in calcTW.
@@ -245,18 +250,19 @@ def calcTW(T, Td, P, satVP):
 #
 def calcDe(T, TW, expSatVP, expDewptVP, fp):
     "Calculate de from temp, estimated "
-    de = T-TW
+    de = T - TW
     de *= fp
     de += expDewptVP
     de -= expSatVP
     return de
 
+
 def reestimateTW(TW, expSatVP, de, fp):
     "Find a new estimate for TW based on the current estimate, \
     the saturation vapor pressure, de, and fp."
-    s = -c2/(TW*TW)
+    s = -c2 / (TW * TW)
     s += c1
     s *= expSatVP
     s -= fp
-    newTW = TW - de/s
+    newTW = TW - de / s
     return newTW
