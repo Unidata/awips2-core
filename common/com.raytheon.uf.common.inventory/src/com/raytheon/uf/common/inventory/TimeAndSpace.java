1d1aa172f1249a631af37686afd69ddf52dfb16b
Shaun Harbison <shaun.harbison@noaa.gov>
2024-03-07 14:50:33 -0600

    Issue #2036814 - Sort the times in LinkedLists for reliable derived parameter results.
    Change-Id: I98e4631d37a49674b760f8e19b33ff5bc314197a
    (cherry picked from commit f0e06f244f7df28dcb1dc85db63ae532006cb78f)

--- common/com.raytheon.uf.common.inventory/src/com/raytheon/uf/common/inventory/TimeAndSpace.java
+++ common/com.raytheon.uf.common.inventory/src/com/raytheon/uf/common/inventory/TimeAndSpace.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -28,23 +28,25 @@ import com.raytheon.uf.common.time.DataTime;
  * Represents a time and space(location) where data can exist. This is used in
  * derived parameters for data types to report when and where data can be
  * loaded.
- * 
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
- * 
+ *
  * Date          Ticket#  Engineer    Description
  * ------------- -------- ----------- --------------------------
  * Apr 11, 2012           bsteffen    Initial creation
  * Apr 11, 2014  2947     bsteffen    Switch space to use IGridGeometryProvider
- * 
+ * Mar 07, 2024  2036814  sharbison   Sort the times in LinkedLists for
+ *                                    reliable derived parameter results.
+ *
  * </pre>
- * 
+ *
  * @author bsteffen
  * @version 1.0
  */
 
-public class TimeAndSpace {
+public class TimeAndSpace implements Comparable<TimeAndSpace> {
 
     /**
      * A constant to represent the time for data that does not change over time
@@ -136,23 +138,30 @@ public class TimeAndSpace {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         TimeAndSpace other = (TimeAndSpace) obj;
         if (space == null) {
-            if (other.space != null)
+            if (other.space != null) {
                 return false;
-        } else if (!space.equals(other.space))
+            }
+        } else if (!space.equals(other.space)) {
             return false;
+        }
         if (time == null) {
-            if (other.time != null)
+            if (other.time != null) {
                 return false;
-        } else if (!time.equals(other.time))
+            }
+        } else if (!time.equals(other.time)) {
             return false;
+        }
         return true;
     }
 
@@ -164,6 +173,38 @@ public class TimeAndSpace {
         sb.append(", Space: ");
         sb.append(space);
         return sb.toString();
+    }
+
+    /**
+     * Sort the times for reliable derived parameter results.
+     */
+    @Override
+    public int compareTo(TimeAndSpace timeAndSpace) {
+        // Compare the forecast time and if equal compare the reference time.
+        // Example of sorted TimeAndSpace:
+        // fcstTime = 583200 refTime = 2024-02-29 12:00:00.0
+        // fcstTime = 583200 refTime = 2024-02-29 06:00:00.0
+        // fcstTime = 604800 refTime = 2024-02-29 12:00:00.0
+        // fcstTime = 604800 refTime = 2024-02-29 06:00:00.0
+
+        if (getTime().getFcstTime() > timeAndSpace.getTime().getFcstTime()) {
+            return 1;
+        } else if (getTime().getFcstTime() == timeAndSpace.getTime()
+                .getFcstTime()) {
+            // If forecast time is equal then compare the reference time.
+            if (getTime().getRefTime().getTime() < timeAndSpace.getTime()
+                    .getRefTime().getTime()) {
+                // Insert the 'newer' refTime first in the results.
+                return 1;
+            } else if (getTime().getRefTime().getTime() == timeAndSpace
+                    .getTime().getRefTime().getTime()) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
     }
 
 }
