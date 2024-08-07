6171ec5e55e51b17a1feee37ff55b19c174e8802
thomas.gurney <Thomas.Gurney@noaa.gov>
2024-08-07 10:58:07 -0500

    Issue #2037700 Faster EDEX shutdown (Camel 4)
    Because Camel uses internally a single-thread executor to run each
    shutdown task, the shutdown of the routes is not parallelized. So it's
    still slow. But I was able to make it a bit faster by stopping all timer
    endpoints before shutdown and by submitting a shutdown task for each
    whole route context rather than each individual route.
    
    Also:
    - Deleted the dummy mode since we don't need it now. Request mode works
      well enough for testing purposes.
    - Gave names to some threads that should have names for debugging
      purposes.
    - Fixed some old typos.
    
    Change-Id: I7525f1665d5fc46afe17ad80099f984829166d4d

--- edex/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/Executor.java
+++ edex/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/Executor.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -24,7 +24,6 @@ import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
@@ -44,7 +43,7 @@ import com.raytheon.uf.edex.esb.camel.context.ContextManager;
 
 /**
  * Provides the central mechanism for starting the ESB
- * 
+ *
  * <pre>
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
@@ -56,7 +55,7 @@ import com.raytheon.uf.edex.esb.camel.context.ContextManager;
  * Jun 12, 2012  #0609     djohnson     Use EDEXUtil for EDEX_HOME.
  * Jul 09, 2012  #0643     djohnson     Read plugin provided resources into system properties.
  * Jul 17, 2012  #0740     djohnson     Redo changes since the decomposed repositories lost them.
- * Oct 19, 2012  #1274     bgonzale     Load properties from files in conf 
+ * Oct 19, 2012  #1274     bgonzale     Load properties from files in conf
  *                                         resources directory.
  * Feb 14, 2013  1638      mschenke     Removing activemq reference in stop
  * Apr 22, 2013  #1932     djohnson     Use countdown latch for a shutdown hook.
@@ -67,8 +66,9 @@ import com.raytheon.uf.edex.esb.camel.context.ContextManager;
  * Feb 08, 2017  6111      njensen      Initialize Spring ApplicationContext more explicitly
  * Jul 10, 2024  2037227   tgurney      Fix EDEX crash when no plugins are present
  *                                      Prevent overriding bean definitions.
+ * Aug 07, 2024  2037700   tgurney      Give the shutdown thread a name
  * </pre>
- * 
+ *
  * @author chammack
  */
 
@@ -86,7 +86,7 @@ public class Executor {
         System.setProperty("System.status", "Starting");
         final AtomicBoolean shutdownContexts = new AtomicBoolean(false);
 
-        Runtime.getRuntime().addShutdownHook(new Thread() {
+        Runtime.getRuntime().addShutdownHook(new Thread("EDEXShutdown") {
             @Override
             public void run() {
                 ContextManager ctxMgr = ContextManager.getInstance();
@@ -101,18 +101,22 @@ public class Executor {
                 if (shutdownContexts.get()) {
                     ctxMgr.stopContexts();
                 } else {
-                    logger.info("Contexts never started, skipping context shutdown");
+                    logger.info(
+                            "Contexts never started, skipping context shutdown");
                 }
 
                 long t2 = System.currentTimeMillis();
                 msg.setLength(0);
-                msg.append("\n**************************************************");
-                msg.append("\n* EDEX ESB is shut down                          *");
+                msg.append(
+                        "\n**************************************************");
+                msg.append(
+                        "\n* EDEX ESB is shut down                          *");
                 msg.append("\n* Total time to shutdown: ")
                         .append(TimeUtil.prettyDuration(t2 - t1)).append("");
                 msg.append("\n* EDEX ESB uptime: ")
                         .append(TimeUtil.prettyDuration(t2 - t0)).append("");
-                msg.append("\n**************************************************");
+                msg.append(
+                        "\n**************************************************");
                 logger.info(msg.toString());
                 shutdownLatch.countDown();
             }
@@ -123,15 +127,15 @@ public class Executor {
         List<File> propertiesFiles = new ArrayList<>();
         File confDir = new File(EDEXModesUtil.CONF_DIR);
         File resourcesDir = new File(confDir, "resources");
-        propertiesFiles.addAll(Arrays.asList(findFiles(resourcesDir,
-                ".properties")));
+        propertiesFiles
+                .addAll(Arrays.asList(findFiles(resourcesDir, ".properties")));
         // load site files after loading the config files so that their
         // properties take precedence.
         String site = System.getProperty("aw.site.identifier");
-        File siteResourcesDir = new File(confDir, "resources" + File.separator
-                + "site" + File.separator + site);
-        propertiesFiles.addAll(Arrays.asList(findFiles(siteResourcesDir,
-                ".properties")));
+        File siteResourcesDir = new File(confDir,
+                "resources" + File.separator + "site" + File.separator + site);
+        propertiesFiles.addAll(
+                Arrays.asList(findFiles(siteResourcesDir, ".properties")));
 
         // Add each file to the system properties
         for (File propertiesFile : propertiesFiles) {
@@ -147,13 +151,13 @@ public class Executor {
             String name = f.getName();
 
             xmlFiles.add(name);
-            springList.add(EDEXModesUtil.XML_PATTERN.matcher(name).replaceAll(
-                    ""));
+            springList.add(
+                    EDEXModesUtil.XML_PATTERN.matcher(name).replaceAll(""));
         }
 
         String modeName = System.getProperty("edex.run.mode");
 
-        if ((modeName != null) && (modeName.length() > 0)) {
+        if (modeName != null && modeName.length() > 0) {
             logger.info("EDEX run configuration: " + modeName);
         } else {
             logger.info("EDEX run configuration must be specified. "
@@ -163,8 +167,8 @@ public class Executor {
         logger.info("EDEX site configuration: "
                 + System.getProperty("aw.site.identifier"));
 
-        List<String> discoveredPlugins = EDEXModesUtil.extractSpringXmlFiles(
-                xmlFiles, modeName);
+        List<String> discoveredPlugins = EDEXModesUtil
+                .extractSpringXmlFiles(xmlFiles, modeName);
 
         StringBuilder msg = new StringBuilder(1000);
         msg.append("\n\nEDEX configuration files: ");
@@ -211,8 +215,8 @@ public class Executor {
         msg.setLength(0);
         msg.append("\n**************************************************");
         msg.append("\n* EDEX ESB is now operational                    *");
-        msg.append("\n* Total startup time: ").append(
-                TimeUtil.prettyDuration(t1 - t0));
+        msg.append("\n* Total startup time: ")
+                .append(TimeUtil.prettyDuration(t1 - t0));
         msg.append("\n**************************************************");
         logger.info(msg.toString());
         msg = null;
@@ -224,7 +228,7 @@ public class Executor {
 
     /**
      * Finds all files in the specified directory with specified extension.
-     * 
+     *
      * @param directory
      *            the directory
      * @param extension
@@ -232,15 +236,11 @@ public class Executor {
      * @return the file array
      */
     private static File[] findFiles(File directory, final String extension) {
-        File[] files = directory.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.getName().endsWith(extension);
-            }
-        });
+        File[] files = directory.listFiles((FileFilter) pathname -> pathname
+                .getName().endsWith(extension));
 
         // If no files were found return an empty array
-        return (files == null) ? new File[0] : files;
+        return files == null ? new File[0] : files;
     }
 
     private static String printList(List<String> components) {
