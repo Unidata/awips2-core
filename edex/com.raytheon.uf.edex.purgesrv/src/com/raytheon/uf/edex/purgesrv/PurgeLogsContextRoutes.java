15f9857de131a4299b2e172cedaa70caba0f9546
adam.ford <adam.ford@noaa.gov>
2024-08-09 14:31:36 -0500

    Issue #2037796 - Converted purge-logs (registry and ingest shared) for Camel 4
    Change-Id: I6464bf128c0d08cda72f4d6222cb77c47f8870bf

---
+++ edex/com.raytheon.uf.edex.purgesrv/src/com/raytheon/uf/edex/purgesrv/PurgeLogsContextRoutes.java
@@ -0,0 +1,55 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ *
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ *
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ *
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+
+package com.raytheon.uf.edex.purgesrv;
+
+import com.raytheon.uf.edex.esb.camel.EDEXRouteBuilder;
+
+/**
+ * Camel routes converted from file "purge-logs.xml", context "purgeLogsContext"
+ *
+ * <pre>
+ *
+ * SOFTWARE HISTORY
+ *
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * 2024-07-11   2037796    aford       Initial creation (from auto-generated)
+ *
+ * </pre>
+ */
+
+public class PurgeLogsContextRoutes extends EDEXRouteBuilder {
+
+    private final String purgeLogsCron;
+
+    public PurgeLogsContextRoutes(String purgeLogsCron) {
+        this.purgeLogsCron = purgeLogsCron;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        // @formatter:off
+        from("quartz://purge/purgeLogScheduled/?cron=" + this.purgeLogsCron)
+              .bean("purgeLogs", "purge")
+              .setId("purgeLogScheduled");
+        // @formatter:on
+    }
+}
