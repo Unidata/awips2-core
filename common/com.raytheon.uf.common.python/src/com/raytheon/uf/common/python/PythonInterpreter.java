1c645df60a471a251f8beb0e8e1032c50e2cde9c
joseph.kelmer <joseph.kelmer@noaa.gov>
2024-09-12 09:41:58 -0400

    Issue #2037920 - Added logging to PythonInterpreter dispose()
    Change-Id: I716de6600119d51816603bf775a33b66f313f79b

--- common/com.raytheon.uf.common.python/src/com/raytheon/uf/common/python/PythonInterpreter.java
+++ common/com.raytheon.uf.common.python/src/com/raytheon/uf/common/python/PythonInterpreter.java
@@ -27,6 +27,9 @@ import jep.JepConfig;
 import jep.JepException;
 import jep.NamingConventionClassEnquirer;
 
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+
 /**
  * Interfaces to a native Python interpreter with Jep.
  *
@@ -47,6 +50,7 @@ import jep.NamingConventionClassEnquirer;
  * Sep 25, 2017   6457     randerso    Add scipy.constants as shared module
  * Dec 19, 2017   7149     njensen     Get shared modules from config file
  * Jun 03, 2019   7852     dgilling    Update code for jep 3.8.
+ * Aug 12, 2024   2037920  jkelmer     Added timing logging to dispose method
  *
  * </pre>
  *
@@ -61,6 +65,9 @@ public abstract class PythonInterpreter implements AutoCloseable {
             + "         g[i] = None\n\n";
 
     protected Jep jep;
+
+    private static final IUFStatusHandler statusHandler =
+    		UFStatus.getHandler(PythonInterpreter.class);
 
     /**
      * Constructor
@@ -184,8 +191,15 @@ public abstract class PythonInterpreter implements AutoCloseable {
      * @throws JepException
      */
     public void dispose() throws JepException {
+        //JFK - added timing logging to identify performance issues
+        long t0 = System.currentTimeMillis();
         cleanupGlobals();
+        long t1 = System.currentTimeMillis();
         jep.close();
+        long t2 = System.currentTimeMillis();
+
+        statusHandler.info("cleanupGlobals took: "+(t1-t0)+" ms");
+        statusHandler.info("jep.Close took: "+(t2-t1)+" ms");
     }
 
     public void cleanupGlobals() {
